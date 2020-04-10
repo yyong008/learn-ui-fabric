1f8004e882befedd0ee9511399fd5abb0b5c1ad9
Magnesium- <757057999@qq.com>
2020-04-10 23:13:06 +0800

    deploy

---
+++ precache-manifest.9e39c59159af84e3c4afd729873e4569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68fdaeb3d9b92a5af4de5ceefd102c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b508111a9517b4b6ba00",
+    "url": "/static/css/main.45f77eb1.chunk.css"
+  },
+  {
+    "revision": "b03f0080c9f7899e552f",
+    "url": "/static/js/2.b83b0066.chunk.js"
+  },
+  {
+    "revision": "6d022101668534bb0e16181e306a572b",
+    "url": "/static/js/2.b83b0066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b508111a9517b4b6ba00",
+    "url": "/static/js/main.708ca34f.chunk.js"
+  },
+  {
+    "revision": "b0affdad4ca989920e2d",
+    "url": "/static/js/runtime-main.284caace.js"
+  }
+]);
